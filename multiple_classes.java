0ce31b48320da1cf3fca83bb7859c568cd18ce7a
Maleficient <48626335+swarnakrishnan@users.noreply.github.com>
2020-10-13 20:56:56 +0530

    multiple classes

---
+++ multiple_classes.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class multiple_classes {
+   public static void main(String args[]){
+       Scanner swarna = new Scanner(System.in);
+       globe globeObject = new globe("Jatin");
+       globeObject.saying();
+   }
+}
+
