8b97d02ea11baf47ba53143d7fd0b779ebfd8e6f
Maleficient <48626335+swarnakrishnan@users.noreply.github.com>
2020-10-13 14:46:58 +0530

    Add files via upload

---
+++ third.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class calculator {
+    public static void main(String args[]){
+        Scanner swarna = new Scanner(System.in);
+        double fnum, snum, answer, diff, mul;
+        System.out.println("Enter first number: ");
+        fnum = swarna.nextDouble();
+        System.out.println("Enter Second number: ");
+        snum = swarna.nextDouble();
+        answer = fnum + snum ;
+        System.out.println(answer);
+        diff = fnum - snum;
+        System.out.println(diff);
+        mul = fnum* snum;
+        System.out.println(mul);
+       }   
+}
